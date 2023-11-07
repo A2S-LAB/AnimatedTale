11828f0b84b96dde4ad4ba13d1e854e1c23f764a
ming-90 <minki@naver.com>
2023-11-07 15:47:54 +0000

    sucess draw circle

--- examples/templates/js/util.js
+++ examples/templates/js/util.js
@@ -17,6 +17,46 @@ const get_skeleton = () => {
             svg.css("display", "block")
             svg.width(width + "px")
             svg.height(height + "px")
+
+            draw_coordinate(result)
         }
     })
+}
+
+const draw_coordinate = (e) => {
+    coor = e.coordinate
+    shape = e.shape
+    width_rate = $("#uploadedImage").width() / shape[0]
+    height_rate = $("#uploadedImage").height() / shape[1]
+
+    $.each(coor, function(idx, val){
+        console.log(val)
+        info = {}
+        info.id = idx
+        info.x = val[0] * width_rate
+        info.y = val[1] * height_rate
+
+        draw_circle(info)
+    })
+}
+
+const draw_circle = (info) => {
+    let tagString =
+        `<circle
+            id='${info.id}'
+            cx='${info.x}'
+            cy='${info.y}'
+            fill='#ff0000'
+            r='6'
+        />`
+    document.getElementById('svg').appendChild(parseSVG(tagString))
+}
+
+const parseSVG = (s) => {
+    let div= document.createElementNS('http://www.w3.org/1999/xhtml', 'div')
+    div.innerHTML= '<svg xmlns="http://www.w3.org/2000/svg">'+s+'</svg>'
+    let frag= document.createDocumentFragment()
+    while (div.firstChild.firstChild)
+        frag.appendChild(div.firstChild.firstChild)
+    return frag
 }
