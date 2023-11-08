360913377c4f604ed93920e2c35404a38a7bc36a
“ming-90” <minki319@naver.com>
2023-11-08 15:27:20 +0000

    sucess circle move

--- examples/templates/js/util.js
+++ examples/templates/js/util.js
@@ -1,3 +1,7 @@
+let mouse_state = 0     // 0 : mouse up , 1 : mouse down
+let mouse_coor = {x:0, y:0}
+let select_circle = -1
+
 const get_skeleton = () => {
     var form = new FormData();
     form.append( "file", $("#fileInput")[0].files[0] );
@@ -24,19 +28,22 @@ const get_skeleton = () => {
 }
 
 const draw_coordinate = (e) => {
+    $('circle').remove()
     coor = e.coordinate
     shape = e.shape
     width_rate = $("#uploadedImage").width() / shape[0]
     height_rate = $("#uploadedImage").height() / shape[1]
 
     $.each(coor, function(idx, val){
-        console.log(val)
         info = {}
         info.id = idx
         info.x = val[0] * width_rate
         info.y = val[1] * height_rate
 
         draw_circle(info)
+        document.getElementById(`${idx}`).addEventListener('mousedown', function(){
+            select_circle = idx
+        })
     })
 }
 
@@ -50,6 +57,10 @@ const draw_circle = (info) => {
             r='6'
         />`
     document.getElementById('svg').appendChild(parseSVG(tagString))
+}
+
+const move_circle = (info) => {
+    select_circle = info
 }
 
 const parseSVG = (s) => {
